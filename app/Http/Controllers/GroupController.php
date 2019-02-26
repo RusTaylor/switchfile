197f5b71ed973d859006121558bc41e2122e3c8f
cube <rustaylor.web@gmail.com>
2019-02-27 01:20:54 +0300

    update

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -16,7 +16,7 @@ class GroupController extends Controller
             "group" => $group,
             "course" => $id
         ])->get();
-        // Доделать контроллер и проверить его(для изменения в будущем)
+        // Доделать контроллер и проверить его(для изменения)
         return view('home', compact('materials'));
     }
 }
