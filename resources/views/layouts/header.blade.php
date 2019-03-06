7df69948de56d4397b04081ebf15111654fdf37d
cube <rustaylor.web@gmail.com>
2019-03-06 15:40:32 +0300

    Update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,29 +1,48 @@
-{{--<header>--}}
-{{--<div class="group-ksk">--}}
-{{--<span>КСК</span>--}}
-{{--</div>--}}
-{{--<ul class="courses-ksk">--}}
-{{--<li><a href="{{url('ksk/2')}}">2 Курс</a></li>--}}
-{{--<li><a href="{{url('ksk/3')}}">3 Курс</a></li>--}}
-{{--<li><a href="{{url('ksk/4')}}">4 Курс</a></li>--}}
-{{--</ul>--}}
-{{--<div class="group-ra">--}}
-{{--<span>РА</span>--}}
-{{--</div>--}}
-{{--<ul class="courses-ra">--}}
-{{--<li><a href="{{url('ra/2')}}">2 Курс</a></li>--}}
-{{--<li><a href="{{url('ra/3')}}">3 Курс</a></li>--}}
-{{--<li><a href="{{url('ra/4')}}">4 Курс</a></li>--}}
-{{--</ul>--}}
-{{--</header>--}}
-
+{{--<nav class="navbar navbar-expand-lg bg-primary">--}}
+    {{--<div class="left">--}}
+        {{--<div class="collapse navbar-collapse" id="navbarNavDropdown">--}}
+            {{--<ul class="navbar-nav">--}}
+                {{--<li class="nav-item dropdown">--}}
+                    {{--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"--}}
+                       {{--aria-haspopup="true" aria-expanded="false" style="font-size: 15px">--}}
+                        {{--Группа КСК--}}
+                    {{--</a>--}}
+                    {{--<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">--}}
+                        {{--<a class="dropdown-item" href="{{url('ksk/2')}}">КСК-2</a>--}}
+                        {{--<a class="dropdown-item" href="{{url('ksk/3')}}">КСК-3</a>--}}
+                        {{--<a class="dropdown-item" href="{{url('ksk/4')}}">КСК-4</a>--}}
+                    {{--</div>--}}
+                {{--</li>--}}
+                {{--<li class="nav-item dropdown">--}}
+                    {{--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"--}}
+                       {{--aria-haspopup="true" aria-expanded="false" style="font-size: 15px">--}}
+                        {{--Группа РА--}}
+                    {{--</a>--}}
+                    {{--<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">--}}
+                        {{--<a class="dropdown-item" href="{{url('ra/2')}}">РА-2</a>--}}
+                        {{--<a class="dropdown-item" href="{{url('ra/3')}}">РА-3</a>--}}
+                        {{--<a class="dropdown-item" href="{{url('ra/4')}}">РА-4</a>--}}
+                    {{--</div>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</nav>--}}
 <nav class="navbar navbar-expand-lg bg-primary">
-    <div class="left">
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{Request::root()}}">Главная <span class="sr-only">(current)</span></a>
+                </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false" style="font-size: 15px">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Группа КСК
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -33,8 +52,7 @@
                     </div>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false" style="font-size: 15px">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Группа РА
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -44,6 +62,9 @@
                     </div>
                 </li>
             </ul>
+            <span class="navbar-text">
+        Navbar text with an inline element
+      </span>
         </div>
     </div>
 </nav>
