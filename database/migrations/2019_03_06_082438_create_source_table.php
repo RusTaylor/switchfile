4e7a61f58e8558744d69b75050f4551b46c86111
cube <rustaylor.web@gmail.com>
2019-03-06 15:01:46 +0300

    Update route and model and migrate

---
+++ database/migrations/2019_03_06_082438_create_source_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSourceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('source', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('group');
+            $table->integer('course');
+            $table->integer('resource_id');
+            $table->string('name_file');
+            $table->string('to_way');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('source');
+    }
+}
