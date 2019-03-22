5954f12d87b4044d36eb677f91e9066abaa4a6fd
RusTaylor <42716037+RusTaylor@users.noreply.github.com>
2019-03-22 12:52:41 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,9 @@
 @include('layouts.header')
 @include('layouts.content')
 @include('layouts.footer')
-<script src="{{asset('css/assets/js/core/jquery.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('css/assets/js/core/popper.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('css/assets/js/core/bootstrap-material-design.min.js')}}" type="text/javascript"></script>
-<script src="{{asset('css/assets/js/plugins/moment.min.js')}}" type="text/javascript"></script>
+<script src="{{secure_asset('css/assets/js/core/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{secure_asset('css/assets/js/core/popper.min.js')}}" type="text/javascript"></script>
+<script src="{{secure_asset('css/assets/js/core/bootstrap-material-design.min.js')}}" type="text/javascript"></script>
+<script src="{{secure_asset('css/assets/js/plugins/moment.min.js')}}" type="text/javascript"></script>
 </body>
 </html>
