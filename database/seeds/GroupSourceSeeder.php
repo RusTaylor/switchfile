85fe5e24898b24a07ed931b3098ea1ebc8b722de
RusTaylor <rustaylor.web@gmail.com>
2019-08-17 22:09:44 +0300

    rename

--- database/seeds/GroupSourceSeeder.php
+++ database/seeds/GroupSourceSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class GroupSource extends Seeder
+class GroupSourceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
