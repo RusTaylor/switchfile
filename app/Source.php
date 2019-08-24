b8ba7d523021eba06ba87b3d7bd13aaed991e37c
RusTaylor <rustaylor.web@gmail.com>
2019-08-24 17:27:28 +0300

    refactor getSources method

--- app/Source.php
+++ app/Source.php
@@ -3,20 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Source extends Model
 {
     protected $table = 'source';
 
-    public static function getSource(string $group, int $course, int $source_id)
+    public static function getSources(int $sourceId)
     {
-        $result = self::where([
-                    'group' => $group,
-                    'course' => $course,
-                    'resource_id' => $source_id
-                ])->get();
-
-        return $result;
+        $sources = DB::table('source as s')
+            ->select(['s.name_file', 's.preview', 's.to_way'])
+            ->join('group_source as gs', 's.resource_id', '=', 'gs.id')
+            ->where('gs.id', '=', $sourceId)
+            ->get();
+        return $sources;
     }
 
 }
