33f55ff696184fea3d56d9621c881d90da371bf7
RusTaylor <rustaylor.web@gmail.com>
2019-08-24 17:27:52 +0300

    fix code style and add TODO

--- resources/views/theme.blade.php
+++ resources/views/theme.blade.php
@@ -1,13 +1,19 @@
 @extends('home')
 @section('content')
     @foreach($sources as $source)
-    <div class="card" style="width: 25rem;float: left;margin-left: 25px">
-        <div class="card-body">
-            <h4 class="card-title">{{$source->name_file}}</h4>
-            <a href="#0" class="card-link"><button type="button" class="btn btn-info">Предпросмотр</button></a>
-            <a href="#0" class="card-link"><button class="btn btn-social btn-fill btn-google">
-                    <i class="fa fa-google-square"></i> Скачать</button></a>
+        <div class="card" style="width: 25rem;float: left;margin-left: 25px">
+            <div class="card-body">
+                <h4 class="card-title">{{$source->name_file}}</h4>
+                {{-- TODO: Подставить в ссылки пути к файлам из бд--}}
+                <a href="#0" class="card-link">
+                    <button type="button" class="btn btn-info">Предпросмотр</button>
+                </a>
+                <a href="#0" class="card-link">
+                    <button class="btn btn-social btn-fill btn-google">
+                        <i class="fa fa-google-square"></i> Скачать
+                    </button>
+                </a>
+            </div>
         </div>
-    </div>
     @endforeach
-    @endsection
+@endsection
