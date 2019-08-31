b0b442f92d660af123e5bf0b0b6857ea7608e20d
RusTaylor <rustaylor.web@gmail.com>
2019-08-31 19:01:06 +0300

    add foreing key for lesson table

--- database/migrations/2019_08_23_214508_add_foreing_key.php
+++ database/migrations/2019_08_23_214508_add_foreing_key.php
@@ -14,11 +14,18 @@ class AddForeingKey extends Migration
     public function up()
     {
         Schema::table('group_source', function (Blueprint $table) {
-            $table->foreign('group_data_id')->references('id')->on('group_data');
+            $table->foreign('group_data_id')->references('id')->on('group_data')->onDelete('cascade');
         });
         Schema::table('source', function (Blueprint $table) {
             $table->foreign('resource_id')->references('id')->on('group_source')->onDelete('cascade');
         });
+        Schema::table('lesson', function (Blueprint $table) {
+            $table->foreign('group_data_id')->references('id')->on('group_data')->onDelete('cascade');
+        });
+        Schema::table('group_data', function (Blueprint $table) {
+            $table->foreign('group_id')->references('id')->on('group');
+        });
+
     }
 
     /**
