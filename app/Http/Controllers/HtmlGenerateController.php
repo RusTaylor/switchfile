6906432c728b3e04ca19a9a804a16bc137f8ab40
RusTaylor <rustaylor.web@gmail.com>
2019-08-31 19:12:20 +0300

    create html generate controller

---
+++ app/Http/Controllers/HtmlGenerateController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Group;
+use App\GroupData;
+use Illuminate\Http\Request;
+
+class HtmlGenerateController extends Controller
+{
+    public static function generateHeader()
+    {
+        $groups = Group::getActiveGroups();
+        $groupsAndCourses = $groups->each(function ($item) {
+            $item->groupCourses = GroupData::getActiveGroupsCourses($item->id);
+        });
+        return $groupsAndCourses;
+    }
+}
