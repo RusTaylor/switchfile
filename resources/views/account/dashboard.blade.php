e6e6d99181e3032b34e3adc4e0773a5196ada7a7
RusTaylor <rustaylor.web@gmail.com>
2019-09-02 14:04:04 +0300

    delete answer code

--- resources/views/account/dashboard.blade.php
+++ resources/views/account/dashboard.blade.php
@@ -44,43 +44,6 @@
                 </td>
             </tr>
         @endforeach
-
-        {{--<tr>--}}
-        {{--<td class="text-center">2</td>--}}
-        {{--<td>John Doe</td>--}}
-        {{--<td>Design</td>--}}
-        {{--<td>2012</td>--}}
-        {{--<td class="text-right">&euro; 89,241</td>--}}
-        {{--<td class="td-actions text-right">--}}
-        {{--<button type="button" rel="tooltip" class="btn btn-info btn-round">--}}
-        {{--<i class="material-icons">person</i>--}}
-        {{--</button>--}}
-        {{--<button type="button" rel="tooltip" class="btn btn-success btn-round">--}}
-        {{--<i class="material-icons">edit</i>--}}
-        {{--</button>--}}
-        {{--<button type="button" rel="tooltip" class="btn btn-danger btn-round">--}}
-        {{--<i class="material-icons">close</i>--}}
-        {{--</button>--}}
-        {{--</td>--}}
-        {{--</tr>--}}
-        {{--<tr>--}}
-        {{--<td class="text-center">3</td>--}}
-        {{--<td>Alex Mike</td>--}}
-        {{--<td>Design</td>--}}
-        {{--<td>2010</td>--}}
-        {{--<td class="text-right">&euro; 92,144</td>--}}
-        {{--<td class="td-actions text-right">--}}
-        {{--<button type="button" rel="tooltip" class="btn btn-info btn-simple">--}}
-        {{--<i class="material-icons">person</i>--}}
-        {{--</button>--}}
-        {{--<button type="button" rel="tooltip" class="btn btn-success btn-simple">--}}
-        {{--<i class="material-icons">edit</i>--}}
-        {{--</button>--}}
-        {{--<button type="button" rel="tooltip" class="btn btn-danger btn-simple">--}}
-        {{--<i class="material-icons">close</i>--}}
-        {{--</button>--}}
-        {{--</td>--}}
-        {{--</tr>--}}
         </tbody>
     </table>
 @endsection
