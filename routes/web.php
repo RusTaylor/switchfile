6d6443ab41c0b345d6c6920c52da5f3fe5d2053e
RusTaylor <rustaylor.web@gmail.com>
2019-09-08 19:48:33 +0300

    create api Route

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,24 @@ Route::get('/{group}/{id}/{sourceId?}', 'GroupController@presentThemes')
     ->where(['group' => '[a-z]+', 'id' => '[2-4]', 'source_id' => '[0-9]+']);
 
 Route::group(['middleware' => 'guest'], function () {
-
     Route::get('/register', 'Auth\RegisterController@showRegistrationForm');
     Route::post('/register', 'Auth\RegisterController@register');
     Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
     Route::post('/login', 'Auth\LoginController@login');
+});
 
+Route::group(['prefix' => 'api'], function () {
+    Route::post('get/courses', 'HtmlGenerateController@ajaxGetCoursesForGroup');
+    Route::post('get/lessons','HtmlGenerateController@ajaxGetLessonsForGroupCourse');
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::post('/get/courses', 'HtmlGenerateController@ajaxGetCoursesForGroup');
+
     Route::group(['prefix' => 'panel'], function () {
         Route::get('/', 'AccountController@index')->name('account');
         Route::get('/create/theme', 'GroupController@viewBlankCreateTheme');
         Route::post('/create/theme', 'GroupController@actionCreateTheme')->name('create_theme');
+
         Route::group(['prefix' => 'action'], function () {
             Route::get('/{id}', 'AccountController@view');
             Route::post('/{id}', 'AccountController@edit');
