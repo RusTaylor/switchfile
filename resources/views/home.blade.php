6cf784590ae8254037ca729c52384b97a939400b
RusTaylor <rustaylor.web@gmail.com>
2020-03-24 18:59:12 +0300

    add link for bootstrap to template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <title>Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{asset('assets/css/alertify.min.css')}}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     {{--<link rel="stylesheet" href="{{asset('assets/css/default.min.css')}}">--}}
     <link rel="stylesheet" href="{{asset('assets/css/material-kit.css')}}">
 </head>
