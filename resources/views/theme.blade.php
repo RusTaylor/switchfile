6645db38c0b4458133eaa8b4ec5576a51ecaca9f
RusTaylor <rustaylor.web@gmail.com>
2020-03-24 19:00:48 +0300

    refactor template

--- resources/views/theme.blade.php
+++ resources/views/theme.blade.php
@@ -1,19 +1,18 @@
 @extends('home')
 @section('content')
+    <div class="container justify-content-center">
     @foreach($sources as $source)
         <div class="card" style="width: 25rem;float: left;margin-left: 25px">
             <div class="card-body">
                 <h4 class="card-title">{{$source->name_file}}</h4>
-                {{-- TODO: Подставить в ссылки пути к файлам из бд--}}
-                <a href="#" class="card-link">
-                    <button type="button" class="btn btn-info">Предпросмотр</button>
-                </a>
-                <a href="#" class="card-link">
-                    <button class="btn btn-social btn-fill btn-google">
+                    <button type="button" class="btn btn-disabled">Предпросмотр</button>
+                <a href="/storage/{{$source->to_way}}" class="card-link">
+                    <button class="btn btn-social btn-fill btn-success">
                         <i class="fa fa-google-square"></i> Скачать
                     </button>
                 </a>
             </div>
         </div>
     @endforeach
+    </div>
 @endsection
